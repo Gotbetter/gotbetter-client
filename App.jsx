4e905ceea74780d1c951577ca9c7d3cc811deab9
yongun2 <djkye0001@naver.com>
2023-07-12 22:58:45 +0900

    [FEAT] 홈화면 라우팅 추가

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '@screens/HomeScreen';
 import SignUpScreen from '@screens/auth/SignUpScreen';
 import React from 'react';
 
@@ -10,7 +11,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="sign-up" component={SignUpScreen} />
       </Stack.Navigator>
