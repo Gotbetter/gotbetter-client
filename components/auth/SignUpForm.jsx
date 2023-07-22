c265b214957739cc44eecf9b864b123c9409651e
yongun2 <djkye0001@naver.com>
2023-07-22 22:48:47 +0900

    [FEAT] 회원가입 기능

--- components/auth/SignUpForm.jsx
+++ components/auth/SignUpForm.jsx
@@ -1,44 +1,95 @@
-import InfoMessage from '@components/common/InfoMessage';
 import ActionButton from '@components/common/btn/ActionButton';
-import React, { useState } from 'react';
+import { ErrorMessage, SuccessMessage } from '@components/common/message';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import styled from 'styled-components/native';
 
-function SignUpForm() {
-  const [isDuplicateChecked] = useState(false);
+SignUpForm.propTypes = {
+  onChange: PropTypes.func,
+  checkDuplicate: PropTypes.func,
+  confirmPassword: PropTypes.func,
+  checkEmailForm: PropTypes.func,
+  state: PropTypes.shape({
+    request: PropTypes.shape({
+      auth_id: PropTypes.string,
+      password: PropTypes.string,
+      passwordConfirm: PropTypes.string,
+      username: PropTypes.string,
+      email: PropTypes.string,
+    }),
+    success: PropTypes.shape({
+      SUCCESS_CHECK_DUPLICATE: PropTypes.bool,
+      SUCCESS_PASSWORD_CONFIRM: PropTypes.bool,
+    }),
+    error: PropTypes.shape({
+      ERROR_CHECK_DUPLICATE: PropTypes.bool,
+      ERROR_PASSWORD_CONFIRM: PropTypes.bool,
+      ERROR_DUPLICATE_EMAIL: PropTypes.bool,
+      ERROR_REQUIRE_UNFULFILLED: PropTypes.bool,
+      ERROR_INCORRECT_EMAIL_FORM: PropTypes.bool,
+    }),
+    message: PropTypes.shape({
+      ERROR_CHECK_DUPLICATE_MESSAGE: PropTypes.string,
+      ERROR_SIGN_UP_MESSAGE: PropTypes.string,
+      ERROR_EMAIL_MESSAGE: PropTypes.string,
+    }),
+  }),
+};
+
+function SignUpForm({ state, onChange, checkDuplicate, confirmPassword, checkEmailForm }) {
+  const {
+    ERROR_CHECK_DUPLICATE,
+    ERROR_PASSWORD_CONFIRM,
+    ERROR_DUPLICATE_EMAIL,
+    ERROR_REQUIRE_UNFULFILLED,
+    ERROR_INCORRECT_EMAIL_FORM,
+  } = state.error;
+  const { SUCCESS_CHECK_DUPLICATE, SUCCESS_PASSWORD_CONFIRM } = state.success;
+  const { ERROR_CHECK_DUPLICATE_MESSAGE, ERROR_SIGN_UP_MESSAGE, ERROR_EMAIL_MESSAGE } = state.message;
 
   return (
     <Form>
       <InputGroup>
         <Label>아이디 *</Label>
         <RowGroup>
-          <Input style={{ width: wp(56) }} placeholder={'아이디'} />
+          <Input style={{ width: wp(56) }} placeholder={'아이디'} onChangeText={(text) => onChange('auth_id', text)} />
           <ActionButton
-            title={isDuplicateChecked ? '확인 완료' : '중복확인'}
+            onPress={checkDuplicate}
+            title={SUCCESS_CHECK_DUPLICATE ? '확인 완료' : '중복확인'}
             width={wp(30)}
             height={hp(8)}
-            color={isDuplicateChecked ? '#263238' : '#E0E0E0'}
+            color={SUCCESS_CHECK_DUPLICATE ? '#263238' : '#E0E0E0'}
             round={true}
+            disabled={SUCCESS_CHECK_DUPLICATE}
           />
         </RowGroup>
-        <InfoMessage message="사용 가능한 아이디입니다." confirmed />
+        <ErrorMessage error={ERROR_CHECK_DUPLICATE} message={ERROR_CHECK_DUPLICATE_MESSAGE} />
+        <SuccessMessage success={SUCCESS_CHECK_DUPLICATE} message={'사용 가능한 아이디입니다.'} />
       </InputGroup>
       <PasswordInputGroup>
         <Label>비밀번호 *</Label>
-        <Input placeholder={'비밀번호'} secureTextEntry />
-        <Input placeholder={'비밀번호 확인'} secureTextEntry />
-        <InfoMessage message="비밀번호가 일치합니다." confirmed />
+        <Input placeholder={'비밀번호'} onChangeText={(text) => onChange('password', text)} secureTextEntry />
+        <Input
+          placeholder={'비밀번호 확인'}
+          onChangeText={(text) => onChange('passwordConfirm', text)}
+          onBlur={confirmPassword}
+          secureTextEntry
+        />
+        <ErrorMessage error={ERROR_PASSWORD_CONFIRM} message={'비밀번호가 일치하지 않습니다.'} />
+        <SuccessMessage success={SUCCESS_PASSWORD_CONFIRM} message={'비밀번호가 일치합니다.'} />
       </PasswordInputGroup>
       <InputGroup>
         <Label>이메일 *</Label>
-        <Input placeholder={'이메일 주소'} />
-        <InfoMessage message="중복된 이메일입니다." error />
+        <Input placeholder={'이메일 주소'} onChangeText={(text) => onChange('email', text)} onBlur={checkEmailForm} />
+        <ErrorMessage error={ERROR_INCORRECT_EMAIL_FORM || ERROR_DUPLICATE_EMAIL} message={ERROR_EMAIL_MESSAGE} />
       </InputGroup>
       <InputGroup>
         <Label>닉네임 *</Label>
-        <Input placeholder={'닉네임'} />
+        <Input placeholder={'닉네임'} onChangeText={(text) => onChange('username', text)} />
       </InputGroup>
+      <ErrorMessage error={ERROR_INCORRECT_EMAIL_FORM || ERROR_REQUIRE_UNFULFILLED} message={ERROR_SIGN_UP_MESSAGE} />
     </Form>
   );
 }
