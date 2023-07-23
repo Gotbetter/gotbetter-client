a07b8bb3a42309f2a90eeae279c2a7b4f245b996
yongun2 <djkye0001@naver.com>
2023-07-23 20:16:13 +0900

    [FEAT] 홈 화면 이동 하면 스터디룸 생성 request 초기화

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -2,10 +2,19 @@ import AndroidSafeAreaView from '@components/common/AndroidSafeAreaView';
 import HomeFooter from '@components/home/HomeFooter';
 import HomeHeader from '@components/home/HomeHeader';
 import StudyRoomList from '@components/home/StudyRoomList';
-import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { useResetRecoilState } from 'recoil';
+import { studyRoomCreateRequest } from 'recoil/room/atoms';
 import StudyRoomListFetcher from 'wrapper/home/StudyRoomListFetcher';
 
 function HomeScreen() {
+  const resetCreateStudyRoomRequest = useResetRecoilState(studyRoomCreateRequest);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    resetCreateStudyRoomRequest();
+  }, [isFocused, resetCreateStudyRoomRequest]);
+
   return (
     <AndroidSafeAreaView>
       <HomeHeader />
