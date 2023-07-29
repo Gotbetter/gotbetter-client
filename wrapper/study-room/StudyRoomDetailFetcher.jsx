1214ee660cb410ecaa2c494d5312e45746fd93ae
yongun2 <djkye0001@naver.com>
2023-07-29 12:00:13 +0900

    [FEAT] 스터디룸에서 사용하는 정보 불러오는 Fetcher

--- wrapper/study-room/StudyRoomDetailFetcher.jsx
+++ wrapper/study-room/StudyRoomDetailFetcher.jsx
@@ -1,53 +1,57 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { fetchStudyRoomDetail } from 'api/room';
 import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import { useQuery } from 'react-query';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { planFetchParamsState } from 'recoil/plan/atoms';
+import { myParticipantIdSelector } from 'recoil/plan/selector';
 import { studyRoomDetail, studyRoomInfoModalState } from 'recoil/room/atoms';
 
 StudyRoomDetailFetcher.propTypes = {
-  roomId: PropTypes.number,
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
-function StudyRoomDetailFetcher({ roomId, children }) {
-  const [studyRoom, setStudyRoomDetail] = useRecoilState(studyRoomDetail);
-  const openModal = useSetRecoilState(studyRoomInfoModalState);
-
+function StudyRoomDetailFetcher({ children }) {
+  const { roomId } = useRoute().params;
   const navigation = useNavigation();
 
-  /**
-   * 헤더 타이틀 세팅
-   */
+  const [studyRoom, setStudyRoomDetail] = useRecoilState(studyRoomDetail);
+  const myParticipantId = useRecoilValue(myParticipantIdSelector);
+
+  const openModal = useSetRecoilState(studyRoomInfoModalState);
+  const setPlanFetchParams = useSetRecoilState(planFetchParamsState);
+
+  /** 스터디룸 헤더 세팅 */
   useEffect(() => {
-    console.log('render header');
     navigation.setOptions({
       title: studyRoom && studyRoom.title,
       headerRight: () => <Feather name={'info'} size={30} onPress={() => openModal(true)} />,
     });
-  }, [navigation, studyRoom]);
+  }, [navigation, openModal, studyRoom]);
 
-  /**
-   * 캐싱된 데이터가 있다면 해당 데이터로 recoil set
-   */
+  /** 캐싱된 데이터가 있다면 해당 데이터로 recoil set */
   useEffect(() => {
-    console.log('render cache');
     if (data !== undefined) {
+      console.log('[StudyRoomDetailFetcher]: caching study room detail');
       setStudyRoomDetail({ ...data });
+      setPlanFetchParams((prev) => ({ ...prev, participantId: myParticipantId, week: data.current_week }));
     }
-  }, [data, setStudyRoomDetail]);
+  }, [data, myParticipantId, setStudyRoomDetail, setPlanFetchParams]);
 
   /**
    * 스터디룸 정보 불러오기
+   * @param roomId: 현재 들어온 스터디룸의 아이디
    */
-  const { isLoading, isError, data } = useQuery(`studyRoomDetail/${roomId}`, () => fetchStudyRoomDetail(roomId), {
+  const { isLoading, isError, data } = useQuery([`studyRoomDetail`, roomId], () => fetchStudyRoomDetail(roomId), {
     retry: 1,
     staleTime: 500000,
     onSuccess: (data) => {
-      console.log('fetch study room detail');
+      console.log('[StudyRoomDetailFetcher]: fetching study room detail');
       setStudyRoomDetail({ ...data });
+      console.log(`set planFetchParams participantId:${myParticipantId} week:${data.current_week}`);
+      setPlanFetchParams((prev) => ({ ...prev, participantId: myParticipantId, week: data.current_week }));
     },
     onError: () => {
       console.log('디테일 에러');
--- wrapper/study-room/StudyRoomParticipantsFetcher.jsx
+++ wrapper/study-room/StudyRoomParticipantsFetcher.jsx
@@ -1,80 +1,70 @@
+import { useRoute } from '@react-navigation/native';
 import { fetchStudyRoomParticipants } from 'api/room';
 import format from 'pretty-format';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Text, View } from 'react-native';
 import { useQuery } from 'react-query';
-import { useSetRecoilState } from 'recoil';
-import { studyRoomJoinRequests, studyRoomParticipants } from 'recoil/room/atoms';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { participantList } from 'recoil/participant/atoms';
+import { studyRoomRefreshState } from 'recoil/room/atoms';
 
 StudyRoomParticipantsFetcher.propTypes = {
-  roomId: PropTypes.number,
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
 
-function StudyRoomParticipantsFetcher({ roomId, children }) {
-  const setParticipants = useSetRecoilState(studyRoomParticipants);
-  const setJoinRequests = useSetRecoilState(studyRoomJoinRequests);
-  /**
-   * 캐싱된 데이터가 있다면 해당 데이터로 recoil set
-   */
+function StudyRoomParticipantsFetcher({ children }) {
+  const refresh = useRecoilValue(studyRoomRefreshState);
+  const setParticipants = useSetRecoilState(participantList);
+
+  const { roomId } = useRoute().params;
+  const accepted = useMemo(() => true, []);
+
+  /** 캐싱된 데이터가 있다면 해당 데이터로 recoil set */
   useEffect(() => {
-    if (participants !== undefined) {
-      setParticipants([...participants]);
-    }
-    if (joinRequests !== undefined) {
-      setJoinRequests([...joinRequests]);
-    }
-  }, [joinRequests, participants, setJoinRequests, setParticipants]);
-
-  /**
-   * 스터디룸 참가자 정보 불러오기
-   */
-  const {
-    isLoading: loadingParticipants,
-    isError: errorParticipants,
-    data: participants,
-  } = useQuery(`studyRoomParticipants/${roomId}`, () => fetchStudyRoomParticipants(roomId, true), {
-    retry: 1,
-    staleTime: 500000,
-    onSuccess: (data) => {
+    if (data !== undefined) {
       setParticipants([...data]);
-    },
-    onError: (err) => {
-      console.log('참가자 에러');
-      console.log(format(err));
-    },
-    select: (res) => res.data,
-  });
+    }
+  }, [data, setParticipants]);
+
+  /** 리프레시 */
+  useEffect(() => {
+    if (refresh) {
+      refetch();
+    }
+  }, [refresh, refetch]);
 
   /**
-   * 스터디룸 참가자 정보 불러오기
+   * 스터디룸 참가자 불러오기
+   * @param roomId: 참여자들이 소속된 스터디룸의 아이디
+   * @param accepted: 스터디룸에 소속되었는지 여부
    */
-  const {
-    isLoading: loadingJoinRequests,
-    isError: errorJoinRequests,
-    data: joinRequests,
-  } = useQuery(`studyRoomJoinRequests/${roomId}`, () => fetchStudyRoomParticipants(roomId, false), {
-    retry: 1,
-    staleTime: 500000,
-    onSuccess: (data) => {
-      setJoinRequests([...data]);
+  const { isLoading, isError, data, refetch } = useQuery(
+    [`studyRoomParticipants`, roomId, accepted],
+    () => fetchStudyRoomParticipants(roomId, accepted),
+    {
+      retry: 1,
+      staleTime: 500000,
+      onSuccess: (data) => {
+        console.log('[StudyRoomParticipantsFetcher]: fetching study room participants');
+        setParticipants([...data]);
+      },
+      onError: (err) => {
+        console.log('참가자 에러');
+        console.log(format(err));
+      },
+      select: (res) => res.data,
     },
-    onError: (err) => {
-      console.log('참가 요청 에러');
-      console.log(format(err));
-    },
-    select: (res) => res.data,
-  });
+  );
 
-  if (loadingJoinRequests || loadingParticipants)
+  if (isLoading)
     return (
       <View>
         <Text>로딩중</Text>
       </View>
     );
 
-  if (errorJoinRequests || errorParticipants)
+  if (isError)
     return (
       <View>
         <Text>에러 발생</Text>
