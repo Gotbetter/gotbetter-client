00064bb2174ff242f8dbaab63c821af68e4f96c3
yongun2 <djkye0001@naver.com>
2023-08-20 02:17:39 +0900

    [DOC] 라우트 Index.js

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+import AuthRoutes from './AuthRoutes';
+import HomeRoutes from './HomeRoutes';
+import PlanRoutes from './PlanRoutes';
+import RoomCreateRoutes from './RoomCreateRoutes';
+import StudyRoomRoutes from './StudyRoomRoutes';
+
+export { AuthRoutes, HomeRoutes, PlanRoutes, RoomCreateRoutes, StudyRoomRoutes };
